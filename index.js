7a36c4ccd17b87ea09a637e90cf624a3ff919be0
Benjamin Kröger <bkroeger@pcgu2.gish.de>
2016-01-14 11:33:52 +0100

    include style rules

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 module.exports = {
   'extends': [
     'eslint-config-airbnb/base',
-    'eslint-config-oniyi/rules/best-practices'
+    'eslint-config-oniyi/rules/best-practices',
+    'eslint-config-oniyi/rules/style'
   ].map(require.resolve),
   rules: {}
 };
