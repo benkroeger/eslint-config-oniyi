13afb44db358299ab8e0d2182e672522e9f25112
Benjamin Kroeger <benjamin.kroeger@gmail.com>
2016-03-16 18:14:40 +0100

    disable prefer-rest-params rule
    feature not supported without babel yet in 4.4.0

--- rules/es6.js
+++ rules/es6.js
@@ -1,5 +1,8 @@
 module.exports = {
   'rules': {
-    'arrow-body-style': [2, "always"]
+    'arrow-body-style': [2, "always"],
+    // http://eslint.org/docs/rules/prefer-rest-params
+    // disable rest params rule, since rest params are not supported in node 4.4.0 and we're not using babel
+    'prefer-rest-params': 0,
   }
 };
