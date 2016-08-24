4cad825fb835cc51b3a8b47f1500836a871e315a
Benjamin Kroeger <benjamin.kroeger@gmail.com>
2016-08-24 11:44:30 +0200

    ignore comments in max-len rule

--- rules/style.js
+++ rules/style.js
@@ -1,10 +1,10 @@
 module.exports = {
-  'rules': {
+  rules: {
     // specify the maximum length of a line in your program
     // http://eslint.org/docs/rules/max-len
     'max-len': [1, 120, 2, {
       ignoreUrls: true,
-      ignoreComments: false
+      ignoreComments: true,
     }],
-  }
+  },
 };
