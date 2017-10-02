53536cc378537a2a155545e5c7e07707029ee9b2
Benjamin Kröger <benjamin.kroeger@gis-ag.com>
2017-10-02 09:37:38 +0200

    changes / simplidies order of `extends`

--- index.js
+++ index.js
@@ -1,15 +1,11 @@
+'use strict';
+
 module.exports = {
   extends: [
-    'eslint-config-airbnb-base/rules/best-practices',
-    'eslint-config-airbnb-base/rules/errors',
+    'eslint-config-airbnb-base',
     './rules/errors', // overload with own errors rules
-    'eslint-config-airbnb-base/rules/node',
     './rules/node', // overload with own node rules
-    'eslint-config-airbnb-base/rules/style',
     './rules/style', // overload with own style rules
-    'eslint-config-airbnb-base/rules/variables',
-    'eslint-config-airbnb-base/rules/es6',
-    'eslint-config-airbnb-base/rules/imports',
   ].map(require.resolve),
   parserOptions: {
     sourceType: 'script',
