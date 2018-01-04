0daee32e32c1b81dad1697bf8e2e50e6e2bd0841
Benjamin Kröger <benjamin.kroeger@gis-ag.com>
2018-01-04 10:24:42 +0100

    force callback-return for `cb`, too

--- rules/node.js
+++ rules/node.js
@@ -2,7 +2,7 @@
 
 module.exports = {
   rules: {
-    'callback-return': [2, ['callback', 'next', 'done']],
+    'callback-return': [2, ['cb', 'callback', 'next', 'done']],
     'handle-callback-err': [2, '^.*(e|E)rr'],
   },
 };
