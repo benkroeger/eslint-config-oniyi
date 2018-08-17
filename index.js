0611c700112bc9334b62aec2b3da23535ffb18c4
Benjamin Kroeger <benjamin.kroeger@gmail.com>
2018-08-17 13:01:42 +0200

    removes custom variables rules file

--- index.js
+++ index.js
@@ -3,10 +3,8 @@
 module.exports = {
   extends: [
     'eslint-config-airbnb-base',
-    './rules/errors', // overload with own errors rules
     './rules/node', // overload with own node rules
     './rules/style', // overload with own style rules
-    './rules/variables', // overload with own style rules
   ].map(require.resolve),
   parserOptions: {
     sourceType: 'script',
--- rules/variables.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-module.exports = {
-  rules: {
-    // disallow use of variables and classes but not functions before they are defined
-    'no-use-before-define': ['error', { functions: false, classes: true, variables: true }],
-  },
-};
